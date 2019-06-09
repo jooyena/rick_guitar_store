1511a7ccafdccc2cb782959346a4b06d12ca60ea
yean <yena26743@gmail.com>
2019-06-09 22:08:55 +0900

    reviese GUI

--- ricksGuitars-final/DB.java
+++ ricksGuitars-final/DB.java
@@ -1,5 +1,7 @@
 import java.sql.*;
 
+import com.sun.javafx.collections.MappingChange.Map;
+
 /*
 --------------+-------------+------+-----+---------+-------+
 | Field        | Type        | Null | Key | Default | Extra |
@@ -22,7 +24,7 @@ public class DB {
 	PreparedStatement state = null;
 
 	public DB() {
-		
+
 		try {
 
 			Class.forName("com.mysql.cj.jdbc.Driver");
@@ -50,8 +52,9 @@ public class DB {
 			state.setString(2, guitar.spec.getModel().toString());
 			state.setString(3, guitar.spec.getType().toString());
 			state.setInt(4, guitar.spec.getNumStrings());
-			state.setString(6, guitar.spec.getBackWood().toString());
 			state.setString(5, guitar.spec.getTopWood().toString());
+			state.setString(6, guitar.spec.getBackWood().toString());
+
 			System.out.println("로드 완료");
 
 		} catch (SQLException e) {
@@ -60,24 +63,42 @@ public class DB {
 	}
 
 	public void Search(GuitarSpec spec) {
-		String sql = "SELECT * FROM rick_store WHERE " + 
-				"builder IN ('" + spec.getBuilder() + "')" +
-				" AND " + "model IN ('" + spec.getModel()+"')" +
-				" AND " + "type IN ('" + spec.getType() + "')" +
-				" AND " +"numStrings IN ('" + spec.getNumStrings()+"')" +
-				" AND " + "backWood IN ('" +spec.getBackWood()+"')" +
-				" AND " + "topWood IN ('"+spec.getTopWood()+"')" ;
-		//mysql 비교문 GuitarSpec의 matching 과 동일한 함수 잘 동작하면 map 타입으로 변환
-		System.out.println(sql);
+		ResultSet result = null;
+		String sql = "SELECT * FROM rick_store WHERE " + "builder IN ('" + spec.getBuilder() + "')" + " AND "
+				+ "model IN ('" + spec.getModel() + "')" + " AND " + "type IN ('" + spec.getType() + "')" + " AND "
+				+ "numStrings IN ('" + spec.getNumStrings() + "')" + " AND " + "backWood IN ('" + spec.getBackWood()
+				+ "')" + " AND " + "topWood IN ('" + spec.getTopWood() + "')";
+		// mysql 비교문 GuitarSpec의 matching 과 동일한 함수 잘 동작하면 map 타입으로 변환
+		// System.out.println(sql);
+		Guitar guitar;
+		try {
+			result = state.executeQuery(sql);
+			while (result.next()) {
+				//guitar = new Guitar(result.getString(8),result.getDouble(7),new GuitarSpec(result.getString(1),result.getString(2),result.getString(3),result.getInt(4),result.getString(5),result.getString(6)));
+				/*result.getString(1);// builder
+				result.getString(2);// model
+				result.getString(3);//type
+				result.getInt(4);//numString
+				result.getString(5);//topWood
+				result.getString(6);//BackWood
+				result.getDouble(7);//price
+				result.getString(8);//serialNumber*/
+			}
+			//enum 타입 없애 버리자...! 그래도 되는지는 모르겠지만
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
 	}
+
 	public void Close() {
-				try {
-					if (conn != null && !conn.isClosed()) {
-						conn.close();
-					}
-				} catch (SQLException e) {
-					e.printStackTrace();
-				}
+		try {
+			if (conn != null && !conn.isClosed()) {
+				conn.close();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
